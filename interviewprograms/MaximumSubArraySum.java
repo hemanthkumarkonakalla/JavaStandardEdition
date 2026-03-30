a547c54fabfa4af0efa38de6ce17b01a7c9d922e
HemanthKumarKonakalla <hemanth.konakalla09@gmail.com>
2026-03-30 22:47:28 +0530

    Core Java

---
+++ interviewprograms/MaximumSubArraySum.java
@@ -0,0 +1,31 @@
+package com.java.standard.edition.interviewprograms;
+
+public class MaximumSubArraySum
+{
+	public static void main(String[] args) 
+	{
+		int arr[]= {1,2,3,4,5,6};
+		int k=3;
+		int sum=0;
+		for(int i=0;i<k;i++)//3<3
+		{
+			sum+=arr[i];
+		}
+		
+		int maxSum=sum;
+		
+		for(int i=k;i<arr.length;i++)//5<6
+		{
+			sum+=arr[i];
+			sum-=arr[i-k];
+			if(sum>maxSum)
+			{
+				maxSum=sum;
+			}
+		}
+		
+		System.out.println("The maximum sub array sum is :"+maxSum);
+		
+	}
+
+}
